7a9f811543cd28fde5a0b04619c86893af470686
Bryan Stewart <bryan.stewart@Bryans-MacBook-Pro.local>
2020-01-13 06:43:22 -0500

    added sms_ok to allowed params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
  protected
 
  def configure_permitted_parameters
- 	attributes = [:fname, :lname, :address, :address2, :city, :state, :zip, :carrier_id, :avatar, :telephone]
+ 	attributes = [:fname, :lname, :address, :address2, :city, :state, :zip, :carrier_id, :avatar, :telephone,:sms_ok]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
  end
