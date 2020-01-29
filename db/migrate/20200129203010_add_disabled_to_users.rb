0cef104954c116216786734af554ed8fdf65e623
Bryan Stewart <bstewart@lolconcepts.com>
2020-01-29 15:31:49 -0500

    added disabled to users

---
+++ db/migrate/20200129203010_add_disabled_to_users.rb
@@ -0,0 +1,8 @@
+class AddDisabledToUsers < ActiveRecord::Migration[5.2]
+  def up
+    add_column :users, :disabled, :boolean, default: false
+  end
+  def down
+  	remove_column :users, :disabled
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_29_160118) do
+ActiveRecord::Schema.define(version: 2020_01_29_203010) do
 
   create_table "carriers", force: :cascade do |t|
     t.string "name"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 2020_01_29_160118) do
     t.integer "carrier_id"
     t.string "role"
     t.string "skill"
+    t.boolean "disabled", default: false
     t.index ["carrier_id"], name: "index_users_on_carrier_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
