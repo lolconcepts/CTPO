0f8d05a34ff66e27361f675729c7edbbf83bd171
Bryan Stewart <bstewart@lolconcepts.com>
2020-08-05 08:10:02 -0400

    changed sort order for offerings to newest first

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -1,6 +1,6 @@
 class OfferingsController < ApplicationController
 	def index
-		@offerings = Offering.all.where(created_at: Time.zone.now.beginning_of_year..Time.zone.now.end_of_day)
+		@offerings = Offering.all.where(created_at: Time.zone.now.beginning_of_year..Time.zone.now.end_of_day).order(created_at: :desc)
 	end
 	def new
 	end
