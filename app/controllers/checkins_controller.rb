e26ca651f5b8ae5a0fc85cd5c17c64fc46d637c0
Bryan Stewart <bstewart@lolconcepts.com>
2020-08-28 14:20:28 -0400

    made alert atendees a red button, added prayer request BAB

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -49,13 +49,13 @@ class CheckinsController < ApplicationController
 
   def checkin
     if params[:uid]
-      user = User.find(params[:uid])
+      userid = User.find(params[:uid])
     else
-      user = current_user
+      userid = current_user
     end
 
   if current_user
-    @checkin = Checkin.new(:user => user, :short_date => Time.now.strftime("%m/%d/%Y"))
+    @checkin = Checkin.new(:user => userid, :short_date => Time.now.strftime("%m/%d/%Y"))
     respond_to do |format|
       if @checkin.save
         format.html { redirect_to root_url, notice: 'Check-In Successful!' }
